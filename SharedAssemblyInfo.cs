468c9a2cb7728ed165cb93187750cb0043dc0d08
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-11-30 15:16:07 +0900

    Assembly info

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aftnet")]
+[assembly: AssemblyProduct("FileSystem Xamarin Plugin")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
