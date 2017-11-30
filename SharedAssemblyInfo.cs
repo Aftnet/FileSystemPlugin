eb77fab25f19ddc2f48b6dde03cd4a6da18893a8
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-11-30 15:35:30 +0900

    logic

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,6 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -10,4 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
