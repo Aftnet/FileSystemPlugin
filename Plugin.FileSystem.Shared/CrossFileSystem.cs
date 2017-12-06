9412394043716e6064c5517760483bb2184b88b1
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-12-06 14:56:20 +0900

    Fixed nuget package

--- Plugin.FileSystem.Shared/CrossFileSystem.cs
+++ Plugin.FileSystem.Shared/CrossFileSystem.cs
@@ -1,10 +1,37 @@
 ﻿using Plugin.FileSystem.Abstractions;
+using System;
+using System.Threading;
 
 namespace Plugin.FileSystem
 {
     public static class CrossFileSystem
     {
-        private static FileSystem current = new FileSystem();
-        public static IFileSystem Current => current;
+#if !NETSTANDARD1_3
+        private static Lazy<FileSystem> fileSystem = new Lazy<FileSystem>(LazyThreadSafetyMode.PublicationOnly);
+#endif
+
+        public static bool Supported
+        {
+            get
+            {
+#if NETSTANDARD1_3
+                return false;
+#else
+                return true;
+#endif
+            }
+        }
+
+        public static IFileSystem Current
+        {
+            get
+            {
+#if NETSTANDARD1_3
+        throw new NotImplementedException();
+#else
+                return fileSystem.Value;
+#endif
+            }
+        }
     }
 }
