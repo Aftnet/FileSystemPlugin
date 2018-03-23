8caa3bdda5f6891212fc9c7e43b3bd217795aa58
claudiuslollarius <claudiuslollarius@gmail.com>
2018-03-24 00:31:14 +0900

    Android fixes

--- Plugin.FileSystem.Android/FileSystem.cs
+++ Plugin.FileSystem.Android/FileSystem.cs
@@ -1,23 +1,13 @@
 ﻿using Plugin.FileSystem.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Plugin.FileSystem
 {
     internal class FileSystem : FileSystemBase
     {
-        public override IDirectoryInfo LocalStorage => GetSpecialFolder(Environment.SpecialFolder.LocalApplicationData);
+        public override IDirectoryInfo LocalStorage => new DirectoryInfo(new System.IO.DirectoryInfo(Android.App.Application.Context.FilesDir.AbsolutePath));
 
-        public override IDirectoryInfo RoamingStorage => GetSpecialFolder(Environment.SpecialFolder.ApplicationData);
+        public override IDirectoryInfo RoamingStorage => LocalStorage;
 
         public override IDirectoryInfo InstallLocation => new DirectoryInfo(new System.IO.DirectoryInfo(Android.App.Application.Context.ApplicationInfo.DataDir));
-
-        private IDirectoryInfo GetSpecialFolder(Environment.SpecialFolder specialFolder)
-        {
-            var path = Environment.GetFolderPath(specialFolder);
-            var folder = new System.IO.DirectoryInfo(path);
-            return new DirectoryInfo(folder);
-        }
     }
 }
