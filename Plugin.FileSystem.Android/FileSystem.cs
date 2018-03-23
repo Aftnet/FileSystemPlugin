a7a5e16eff30fa4fe67bb601e0919208100bfc92
Alberto Fustinoni <claudiuslollarius@gmail.com>
2018-03-23 14:10:16 +0900

    Logic

--- Plugin.FileSystem.Android/FileSystem.cs
+++ Plugin.FileSystem.Android/FileSystem.cs
@@ -7,17 +7,10 @@ namespace Plugin.FileSystem
 {
     internal class FileSystem : FileSystemBase
     {
-        public override IDirectoryInfo LocalStorage => GetSpecialFolder(Environment.SpecialFolder.LocalApplicationData);
+        public override IDirectoryInfo LocalStorage => new DirectoryInfo(new System.IO.DirectoryInfo(Android.App.Application.Context.FilesDir.AbsolutePath));
 
-        public override IDirectoryInfo RoamingStorage => GetSpecialFolder(Environment.SpecialFolder.ApplicationData);
+        public override IDirectoryInfo RoamingStorage => LocalStorage;
 
         public override IDirectoryInfo InstallLocation => new DirectoryInfo(new System.IO.DirectoryInfo(Android.App.Application.Context.ApplicationInfo.DataDir));
-
-        private IDirectoryInfo GetSpecialFolder(Environment.SpecialFolder specialFolder)
-        {
-            var path = Environment.GetFolderPath(specialFolder);
-            var folder = new System.IO.DirectoryInfo(path);
-            return new DirectoryInfo(folder);
-        }
     }
 }
