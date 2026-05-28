1d33c706b64e3388a843f10b4f96920d75e56191
Alberto Fustinoni <alberto@aftnet.net>
2026-05-28 19:14:28 +0900

    Logic

--- Plugin.FileSystem/Shared/FileSystemBase.cs
+++ Plugin.FileSystem/Shared/FileSystemBase.cs
@@ -1,7 +1,4 @@
 ﻿using Plugin.FileSystem.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Plugin.FileSystem
 {
@@ -13,47 +10,57 @@ namespace Plugin.FileSystem
 
         public abstract IDirectoryInfo InstallLocation { get; }
 
-        public virtual Task<IFileInfo> PickFileAsync(IEnumerable<string> extensionsFilter = null)
+        public virtual Task<IFileInfo?> PickFileAsync(IEnumerable<string>? extensionsFilter = null)
         {
             return Task.FromResult(default(IFileInfo));
         }
 
-        public virtual Task<IFileInfo[]> PickFilesAsync(IEnumerable<string> extensionsFilter = null)
+        public virtual Task<IFileInfo[]?> PickFilesAsync(IEnumerable<string>? extensionsFilter = null)
         {
             return Task.FromResult(default(IFileInfo[]));
         }
 
-        public virtual Task<IFileInfo> PickSaveFileAsync(string defaultExtension, string suggestedName = null)
+        public virtual Task<IFileInfo?> PickSaveFileAsync(string defaultExtension, string? suggestedName = null)
         {
             return Task.FromResult(default(IFileInfo));
         }
 
-        public virtual Task<IDirectoryInfo> PickDirectoryAsync()
+        public virtual Task<IDirectoryInfo?> PickDirectoryAsync()
         {
             return Task.FromResult(default(IDirectoryInfo));
         }
 
-        public Task<IFileInfo> GetFileFromPathAsync(string path)
+        public Task<IFileInfo?> GetFileFromPathAsync(string path)
         {
             if (string.IsNullOrEmpty(path))
             {
                 throw new ArgumentException();
             }
 
+            var output = default(IFileInfo);
             var file = new System.IO.FileInfo(path);
-            IFileInfo output = file.Exists ? new FileInfo(file) : null;
+            if (file != null && file.Exists)
+            {
+                output = new FileInfo(file);
+            }
+
             return Task.FromResult(output);
         }
 
-        public Task<IDirectoryInfo> GetDirectoryFromPathAsync(string path)
+        public Task<IDirectoryInfo?> GetDirectoryFromPathAsync(string path)
         {
             if (string.IsNullOrEmpty(path))
             {
                 throw new ArgumentException();
             }
 
+            var output = default(IDirectoryInfo);
             var folder = new System.IO.DirectoryInfo(path);
-            IDirectoryInfo output = folder.Exists ? new DirectoryInfo(folder) : null;
+            if (folder != null && folder.Exists)
+            {
+                output = new DirectoryInfo(folder);
+            }
+
             return Task.FromResult(output);
         }
     }
